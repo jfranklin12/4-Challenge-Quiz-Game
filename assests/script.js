d6b48e839ba04aab3bb7ab9f8fa9896a5dec6b4d
jfranklin12 <j.franks725@gmail.com>
2022-06-28 10:04:40 -0400

    created arrays for questions and answers and creating functions for questions and answers

--- assests/script.js
+++ assests/script.js
@@ -2,49 +2,35 @@ var startButtonEL = document.getElementById("start");
 var questionBoxEl = document.getElementById("questionBox");
 var timerEL = document.getElementById("timer");
 var questionEL = document.getElementById("questions");
-var answersEL = document.getElementById("button")
-var myQuestions = [
-    {
-        question: "Which animal can fly?",
-        answers: {
-            a: 'cat',
-            b: 'dog',
-            c: 'bird',
-            d: 'elephant'
-        },
-        correctAnswer: 'c'        
-    },
-    {
-        question: "What color is the sky?",
-        answers: {
-            a: 'blue',
-            b: 'green',
-            c: 'purple',
-            d: 'orange'
-        },
-        correctAnswer: 'a'        
-    },
-    {
-        question: "How many named oceans are in the world?",
-        answers: {
-            a: '4',
-            b: '5',
-            c: '6',
-            d: '7'
-        },
-        correctAnswer: 'b'        
-    },
-]
+var answers1EL = document.getElementById("answerButton1");
+var answers2EL = document.getElementById("answerButton2");
+var answers3EL = document.getElementById("answerButton3");
+var answers4EL = document.getElementById("answerButton4");
+var myQuestions = ["Which animal can fly?", "What color is the sky?","How many named oceans are in the world?"];
+var myAnswers1 = ["cat","blue","4","?"];
+var myAnswers2 = ["dog","green","5","?"];
+var myAnswers3 = ["bird","purple","6","?"];
+var myAnswers4 = ["elephant","orange","7","?"];
+
 
 function showQuestions() {
+    questionEL.innerText = myQuestions[0];
+    // for(var i; i >= myQuestions.length; i++){}
 
+}
+
+function showAnswers() {
+    answers1EL.innerText = myAnswers1[0];
+    answers2EL.innerText = myAnswers2[0];
+    answers3EL.innerText = myAnswers3[0];
+    answers4EL.innerText = myAnswers4[0];
 
 }
 
 function timerCountdown() {
     var timeLeft = 60;
     var timeInterval = setInterval(function() {
-        if (timeLeft > 1) {
+        if (timeLeft > 0) {
             timerEL.textContent = 'Timer: ' + timeLeft;
             timeLeft--;
         } else {
@@ -59,7 +45,12 @@ function startQuiz() {
     startButtonEL.addEventListener('click', function(startQuiz) {
         startButtonEL.style.display = 'none';
         questionBoxEl.style.display = 'inline';
-    timerCountdown()
+        timerCountdown()
+        showQuestions()
+        showAnswers()
+        // myAnswers1.addEventListener("click", function()) {
+
+        // }
     
     });
 }
